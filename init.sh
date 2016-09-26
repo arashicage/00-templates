20ad14d07b145bb0a8164f8b8c5ed67530d15405
arashicage <arashicage@yeah.net>
2016-09-26 12:58:39 +0800

    no message

---
+++ init.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+mkdir -p cmd
+mkdir -p conf
+mkdir -p docker
+mkdir -p modules/cron
+mkdir -p modules/setting
+mkdir -p orm
+mkdir -p models
+mkdir -p public/asserts
+mkdir -p public/css
+mkdir -p public/fonts
+mkdir -p public/img
+mkdir -p public/js
+mkdir -p public/less
+mkdir -p public/plugins
+mkdir -p routers
+mkdir -p scripts
+mkdir -p templeates
